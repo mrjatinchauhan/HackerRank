194cdd5c05015a593423bc2bcdd1eba13914997e
mrjatinchauhan <jatin.405chauhan@gmail.com>
2020-09-22 19:08:03 +0530

    :file_folder: NEW: File Upload

---
+++ C++/Input and Output.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int i,sum=0;int a[3];
+    for(i=0;i<3;i++)
+    {
+        cin>>a[i];
+        sum=sum+a[i];
+            
+    }
+    cout<<sum;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
